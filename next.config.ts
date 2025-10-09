77515719911e0a5381bef4b197a1295cb9a3c350
ReniUswatun <reniuswatunhasanah@student.uns.ac.id>
2025-10-09 14:55:37 +0700

    change next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // Tambahkan baris ini untuk mengaktifkan output yang dibutuhkan oleh Docker
+  output: "standalone",
 };
 
 export default nextConfig;
