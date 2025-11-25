a22a8a19234d336e7d92f21378257100b2101df4
Menghantui <muh.arsyadrayhan@gmail.com>
2025-11-25 11:20:08 +0700

    fix routes

--- app/api/vouchers/[id]/route.ts
+++ app/api/vouchers/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
-
-const BACKEND_VOUCHER_URL = "http://localhost:5000/voucher";
+const host = process.env.API_HOST;
+const port = process.env.API_PORT;
+const BACKEND_VOUCHER_URL = `http://${host}:${port}/voucher`;
 
 /**
  * GET /api/vouchers/[id] - Get voucher by ID
