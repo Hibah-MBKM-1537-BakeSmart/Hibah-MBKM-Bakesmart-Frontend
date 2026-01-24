6e1f847c5002e07e9b6a24529a91267a4a1766cd
Rafi3 <rafi3ramadhan@gmail.com>
2026-01-24 21:51:50 +0700

    sesuaikan untuk prod

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standalone",
+  
   reactStrictMode: false,
+  swcMinify: true,
+  compress: true,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'merpatisolobakery.id',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+      },
+    ],
+  },
+  experimental: {},
 };
 
 export default nextConfig;
